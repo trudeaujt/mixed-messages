f1e5516cb1466f0143ca423ebe544bd4d5754e99
Jonathan Trudeau <48639690+trudeaujt@users.noreply.github.com>
2022-01-01 10:41:19 +0900

    Update logic

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max)
 }
 
+function getRandomElement(array) {
+    return array[getRandomInt(array.length)]
+}
+
 function getMessage() {
-    return `A ${state[getRandomInt(state.length)]} ${colors[getRandomInt(colors.length)]} ${flowers[getRandomInt(flowers.length)]}`
+    return `A ${getRandomElement(state)} ${getRandomElement(colors)} ${getRandomElement(flowers)}`
 }
 
 console.log(getMessage())
